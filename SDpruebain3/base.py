05600f1ca1b26b4eeb8872c31ecbd94f82c6f015
edferoca <efrojasc@unal.edu.co>
2019-03-06 10:38:04 -0500

    crea sd 3

---
+++ SDpruebain3/base.py
@@ -0,0 +1,152 @@
+from migen import *
+
+from migen.genlib.io import CRG
+
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores import dna, xadc
+from litex.soc.cores.spi import SPIMaster
+
+from ios import Led, RGBLed, Button, Switch
+from display import Display
+
+
+
+
+#
+# platform
+#
+
+_io = [
+    ("user_led",  0, Pins("H17"), IOStandard("LVCMOS33")),
+    ("user_led",  1, Pins("K15"), IOStandard("LVCMOS33")),
+    ("user_led",  2, Pins("J13"), IOStandard("LVCMOS33")),
+    ("user_led",  3, Pins("N14"), IOStandard("LVCMOS33")),
+    ("user_led",  4, Pins("R18"), IOStandard("LVCMOS33")),
+    ("user_led",  5, Pins("V17"), IOStandard("LVCMOS33")),
+    ("user_led",  6, Pins("U17"), IOStandard("LVCMOS33")),
+    ("user_led",  7, Pins("U16"), IOStandard("LVCMOS33")),
+    ("user_led",  8, Pins("V16"), IOStandard("LVCMOS33")),
+    ("user_led",  9, Pins("T15"), IOStandard("LVCMOS33")),
+    ("user_led", 10, Pins("U14"), IOStandard("LVCMOS33")),
+    ("user_led", 11, Pins("T16"), IOStandard("LVCMOS33")),
+    ("user_led", 12, Pins("V15"), IOStandard("LVCMOS33")),
+    ("user_led", 13, Pins("V14"), IOStandard("LVCMOS33")),
+    ("user_led", 14, Pins("V12"), IOStandard("LVCMOS33")),
+    ("user_led", 15, Pins("V11"), IOStandard("LVCMOS33")),
+
+    ("user_btn", 0, Pins("N17"), IOStandard("LVCMOS33")),
+    ("user_btn", 1, Pins("P18"), IOStandard("LVCMOS33")),
+    ("user_btn", 2, Pins("P17"), IOStandard("LVCMOS33")),
+    ("user_btn", 3, Pins("M17"), IOStandard("LVCMOS33")),
+    ("user_btn", 4, Pins("M18"), IOStandard("LVCMOS33")),
+
+    ("display_cs_n",  0, Pins("J17 J18 J14 P14 K2 U13 T9 T14"), IOStandard("LVCMOS33")),
+    ("display_abcdefg",  0, Pins("T10 R10 K16 K13 P15 T11 L18 H15"), IOStandard("LVCMOS33")),
+
+    ("clk100", 0, Pins("E3"), IOStandard("LVCMOS33")),
+
+    ("cpu_reset", 0, Pins("C12"), IOStandard("LVCMOS33")),
+
+    ("serial", 0,
+        Subsignal("tx", Pins("D4")),
+        Subsignal("rx", Pins("C4")),
+        IOStandard("LVCMOS33"),
+    ),
+
+
+    #("sd_spi", 0,
+    #    Subsignal("sdcs_n", Pins("C17")),
+    #    Subsignal("sdclk", Pins("D18")),
+    #    Subsignal("sdmosi", Pins("E18")),
+    #    Subsignal("sdmiso", Pins("G17")),
+    #    IOStandard("LVCMOS33")
+    #),
+
+    ("sd_spi", 0,
+        Subsignal("cs_n", Pins("D14")),
+        Subsignal("clk", Pins("F16")),
+        Subsignal("mosi", Pins("G16")),
+        Subsignal("miso", Pins("H14")),
+        IOStandard("LVCMOS33")
+    ),
+
+]
+
+
+class Platform(XilinxPlatform):
+    default_clk_name = "clk100"
+    default_clk_period = 10.0
+
+    def __init__(self):
+        XilinxPlatform.__init__(self, "xc7a100t-CSG324-1", _io, toolchain="ise")
+
+    def do_finalize(self, fragment):
+        XilinxPlatform.do_finalize(self, fragment)
+
+
+def csr_map_update(csr_map, csr_peripherals):
+    csr_map.update(dict((n, v)
+        for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))
+
+
+#
+# design
+#
+
+# create our platform (fpga interface)
+platform = Platform()
+
+# create our soc (fpga description)
+class BaseSoC(SoCCore):
+    # Peripherals CSR declaration
+    csr_peripherals = [
+        "leds",
+        "buttons",
+        "display",
+        "SD"
+    ]
+    csr_map_update(SoCCore.csr_map, csr_peripherals)
+
+    def __init__(self, platform):
+        sys_clk_freq = int(100e6)
+        # SoC with CPU
+        SoCCore.__init__(self, platform,
+            cpu_type="lm32",
+            clk_freq=100e6,
+            ident="CPU Test SoC", ident_version=True,
+            integrated_rom_size=0x8000,
+            csr_data_width=32,
+            integrated_main_ram_size=16*1024)
+
+        # Clock Reset Generation
+        self.submodules.crg = CRG(platform.request("clk100"), ~platform.request("cpu_reset"))
+
+        # Led
+        user_leds = Cat(*[platform.request("user_led", i) for i in range(16)])
+        self.submodules.leds = Led(user_leds)
+        # Buttons
+        user_buttons = Cat(*[platform.request("user_btn", i) for i in range(5)])
+        self.submodules.buttons = Button(user_buttons)
+
+        # lcd
+        self.submodules.SD = SPIMaster(platform.request("sd_spi"))
+        #sdcard
+
+        # Display
+        self.submodules.display = Display(sys_clk_freq)
+        self.comb += [
+            platform.request("display_cs_n").eq(~self.display.cs),
+            platform.request("display_abcdefg").eq(~self.display.abcdefg)
+        ]
+
+soc = BaseSoC(platform)
+
+#
+# build
+#
+builder = Builder(soc, output_dir="build", csr_csv="test/csr.csv")
+builder.build()
---
+++ SDpruebain3/build/software/libbase/spiflash.d
@@ -0,0 +1,10 @@
+spiflash.o: /opt/liteX/litex/litex/soc/software/libbase/spiflash.c \
+  /home/edferoca/Documentos/digital2/fpga_101-master/lab004/build/software/include/generated/csr.h \
+  /opt/liteX/litex/litex/soc/software/include/base/stdint.h \
+  /opt/liteX/litex/litex/soc/software/include/hw/common.h
+
+/home/edferoca/Documentos/digital2/fpga_101-master/lab004/build/software/include/generated/csr.h:
+
+/opt/liteX/litex/litex/soc/software/include/base/stdint.h:
+
+/opt/liteX/litex/litex/soc/software/include/hw/common.h:
