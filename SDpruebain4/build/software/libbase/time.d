10334059c690ad3a60af7560446a0af60dc6474a
Hadazar <hadazar@unal.edu.co>
2019-03-13 09:46:37 -0500

    SDpruebain4

---
+++ SDpruebain4/build/software/libbase/time.d
@@ -0,0 +1,13 @@
+time.o: /opt/liteX/litex/litex/soc/software/libbase/time.c \
+ /home/hector/digital-II/SDpruebain3/build/software/include/generated/csr.h \
+ /opt/liteX/litex/litex/soc/software/include/base/stdint.h \
+ /opt/liteX/litex/litex/soc/software/include/hw/common.h \
+ /opt/liteX/litex/litex/soc/software/include/base/time.h
+
+/home/hector/digital-II/SDpruebain3/build/software/include/generated/csr.h:
+
+/opt/liteX/litex/litex/soc/software/include/base/stdint.h:
+
+/opt/liteX/litex/litex/soc/software/include/hw/common.h:
+
+/opt/liteX/litex/litex/soc/software/include/base/time.h:
